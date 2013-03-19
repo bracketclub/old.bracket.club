cc11f38b47ac2665fd4bcea320d19b7befdad7fd
Luke Karrys <lukekarrys@gmail.com>
2013-03-19 05:21:09 -0700

    v2 rough

--- app.js
+++ app.js
@@ -7,11 +7,29 @@
  */
 
 var express = require('express'),
-    routes = require('./routes'),
     http = require('http'),
-    path = require('path');
+    path = require('path'),
+    log = require('./lib/log'),
 
-var app = express();
+    appPackage = require('./package'),
+    config = require('./config'),
+    TwitterWatcher = require('./lib/twitter'),
+
+    currentYear = new Date().getFullYear(),
+    lockTime = new Date(config.lockTimes[currentYear]).getTime(),
+    isOpen = function() {
+      var now = new Date().getTime();
+      return (lockTime - now > 0);
+    },
+    bracket = require('bracket-validator')(),
+    Entry = new require('./lib/entryModel'),
+
+    BracketValidator = bracket.validator,
+    consts = bracket.consts,
+    _ = require('underscore'),
+
+    app = express(),
+    twitter = new TwitterWatcher({appName: appPackage.name, tag: config.twitter.hashtags[0]});
 
 app.configure(function () {
   app.set('port', 8080);
@@ -22,7 +40,12 @@ app.configure(function () {
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(express['static'](path.join(__dirname, 'public')));
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(function(req, res, next){
+    res.status(404);
+    var userUrl = /\/user\/(.*)/.exec(req.url);
+    res.render('404', {userUrl: (userUrl) ? userUrl[1] : '', title: '404'});
+  });
 });
 
 app.configure('development', function () {
@@ -33,9 +56,62 @@ app.configure('production', function () {
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
+app.get('/', function(req, res) {
+  var validator = new BracketValidator();
+  validator.validate(function(err, bracketData) {
+    res.render('index', {
+      title: 'Create Your Bracket!',
+      regions: _.omit(bracketData, consts.FINAL_ID),
+      finalRegion: bracketData[consts.FINAL_ID],
+      live: isOpen()
+    });
+  });
+});
+
+app.get('/faq', function(req, res) {
+  res.render('faq', {
+    title: 'Frequently Asked Questions',
+    timeToLocked: lockTime - new Date().getTime()
+  });
+});
+
+app.get('/results', function(req, res) {
+  res.render('results', {
+    title: 'Results',
+    isOpen: isOpen(),
+    hasScores: false
+  });
+});
+
+app.get('/user/:username', function(req, res) {
+  var username = req.params.username;
+  Entry.findOne({username: username}, function(err, entry) {
+    if (err || !entry) {
+      res.status(404);
+      res.render('404', {userUrl: username, title: '404'});
+    } else {
+      var validator = new BracketValidator({flatBracket: entry.bracket});
+      validator.validate(function(err, bracketData) {
+        if (err) {
+          res.status(404);
+          res.render('404', {userUrl: username, title: '404'});
+        } else {
+          res.render('user', {
+            title: '@' + username,
+            regions: _.omit(bracketData, consts.FINAL_ID),
+            finalRegion: bracketData[consts.FINAL_ID],
+            live: isOpen(),
+            entry: entry
+          });
+        }
+      });
+    }
+  });
+});
+
+twitter.start();
 
 http.createServer(app).listen(app.get('port'), function () {
-  console.log("Express server listening on port %d in %s mode", app.get('port'), app.settings.env);
+  log.debug('Express server listening', app.get('port'), app.settings.env);
 });
 
---
+++ public/js/bracket.js
@@ -0,0 +1,132 @@
+/*global Backbone, _ */
+
+
+var app;
+
+var BracketRouter = Backbone.Router.extend({
+  routes: {
+    ':bracket': 'bracket'
+  },
+  initialize: function() {
+    new BracketView();
+  },
+  bracket: function(bracket) {
+    //console.log(bracket);
+  }
+});
+
+var BracketModel = Backbone.Model.extend({
+  defaults: {
+
+  },
+  initialize: function() {
+    this.listenTo('change', this.attrChange);
+  }
+});
+
+var BracketView = Backbone.View.extend({
+  events: {
+    'click a.game': 'gameClick'
+  },
+  el: '#bracket',
+  gameClick: function(e) {
+
+    e.preventDefault();
+
+    var $target = $(e.target),
+        $matchup = $target.parents('.matchup'),
+        $thisRound = $matchup.parents('.round'),
+        $thisRegion = $thisRound.parents('.bracket'),
+        $nextRound = $thisRound.next(),
+        $nextRounds = $thisRound.nextAll(),
+        $winnerGoesTo = $nextRound.find('a').eq($matchup.index());
+
+    if ($winnerGoesTo.data('seed') && $target.data('seed') !== $winnerGoesTo.data('seed')) {
+      $nextRounds.find('[data-seed="' + $winnerGoesTo.data('seed') + '"]').replaceWith('<a>&nbsp;</a>');
+      this.$el.find('.bracket').last()
+        .find('[data-seed="' + $winnerGoesTo.data('seed') + '"][data-from-region="' + $winnerGoesTo.data('from-region') + '"]')
+        .replaceWith('<a>&nbsp;</a>');
+    }
+
+    if ($nextRound.length > 0) {
+      $nextRound.find('a').eq($matchup.index()).replaceWith($target.clone());
+    }
+
+    if ($nextRound.length === $nextRounds.length) {
+      this.$el.find('.bracket').last().find('a').eq($thisRegion.index()).replaceWith($target.clone());
+    }
+
+    var bc = this.getBracketCode();
+    app.navigate('#' + bc, {trigger: false});
+
+    var href = 'https://twitter.com/intent/tweet?hashtags=tybrkt&via=tweetthebracket&url=';
+
+    if (bc.indexOf('X') > -1) {
+      $('#tweet-button').addClass('disabled');
+      $('#tweet-button').attr('href', '').click(function(e){return false;});
+    } else {
+      $('#tweet-button').removeClass('disabled');
+      $('#tweet-button').unbind().attr('href', href + encodeURIComponent(window.location.href));
+    }
+    return false;
+  },
+  getBracketCode: function() {
+    return _.map(this.$el.find('.bracket'), function(item) {
+      return this.getRegionCode(item.id);
+    }, this).join('');
+  },
+  getRegionCode: function(regionId) {
+    var $region = this.$el.find('#' + regionId);
+    return regionId + _.map($region.find('.round:gt(0) a'), function(item) { return ((regionId === 'FF') ? $(item).data('from-region') : $(item).data('seed')) || 'X';}).join('');
+  }
+});
+
+
+(function ($) {
+
+  $(function(){
+
+    app = new BracketRouter();
+    Backbone.history.start({pushState: false});
+
+    $('form.navbar-search').submit(function(e) {
+      e.preventDefault();
+      var username = $(this).find('input').val();
+      if (username) {
+        window.location.href = '/user/' + username;
+      }
+    });
+
+    $('.subnav a').smoothScroll();
+
+    // fix sub nav on scroll
+    var $win = $(window),
+        $body = $('body'),
+        $nav = $('.subnav'),
+        navHeight = $('.navbar').first().height(),
+        subnavTop = $('.subnav').length && $('.subnav').offset().top - navHeight,
+        isFixed = 0;
+
+    if ($('.subnav').length > 0) {
+      processScroll();
+
+      $win.on('scroll', processScroll);
+    }
+
+    function processScroll() {
+      var i, scrollTop = $win.scrollTop();
+
+      if (scrollTop >= subnavTop && !isFixed) {
+        isFixed = 1;
+        $nav.addClass('subnav-fixed');
+        $body.addClass('has-subnav-fixed');
+      } else if (scrollTop <= subnavTop && isFixed) {
+        isFixed = 0;
+        $nav.removeClass('subnav-fixed');
+        $body.removeClass('has-subnav-fixed');
+      }
+    }
+
+  });
+
+})(window.jQuery);
