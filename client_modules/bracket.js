e5e34c5502becbf3a7f5de8e1ba672ddf81e20d6
Luke Karrys <lukekarrys@gmail.com>
2014-01-28 22:47:59 -0700

    keep files for new version

--- client_modules/bracket.js
+++
@@ -1,4 +0,0 @@
-window.Bracket = {
-  Validator: require('../node_modules/bracket-validator/lib/validator'),
-  Generator: require('../node_modules/bracket-validator/lib/generator')
-};
--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
-# tweetyourbracket.com
+tweetyourbracket.com
+===============
