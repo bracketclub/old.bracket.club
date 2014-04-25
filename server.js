b6ee09efd11d39456d638d5cfcd9b61a5c795ad9
Luke Karrys <lukekarrys@gmail.com>
2014-04-24 18:01:01 -0700

    cleanup and a completely unchecked attempt for #56. Im sure this doesnt work, but its so clean!

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 var path = require('path');
 var express = require('express');
 var Moonboots = require('moonboots-express');
+var Static = require('moonboots-static');
+var templatizer = require('templatizer');
+var lessitizer = require('lessitizer');
+var Crawler = require('spa-crawler');
 var expressApp = express();
-var build = require('./build');
 var appName = require('./package').name;
 var jade = require('jade');
 var fs = require('fs');
 var config = require('figs');
+var mkdirp = require('mkdirp');
+var sh = require('execSync');
 var year = process.env.TYB_YEAR || config.year;
 var sport = process.env.TYB_SPORT || config.sport;
 var liveData = require('bracket-data-live')({year: year, sport: sport});
@@ -44,91 +49,94 @@ var htmlSource = function (cb) {
     }));
 };
 
-// -----------------
-// Moonboots variable options
-// -----------------
-var buildDirectory = null;
-var resourcePrefix = '/';
-
-if (argv('build')) {
-    buildDirectory = fixPath('_deploy');
-    resourcePrefix = '/assets/';
-} else if (argv('pages')) {
-    buildDirectory = fixPath('_pages');
-    resourcePrefix = '/assets/';
-}
 
 // ---------------------------------------------------
 // Configure our main route that will serve our moonboots app
 // ---------------------------------------------------
-expressApp.get(resourcePrefix + 'data*.js', function (req, res) {
+expressApp.get('*data*.js', function (req, res) {
     res.set('Content-Type', 'text/javascript; charset=utf-8');
     res.send(dataString);
 });
 
-
-// ---------------------------------------------------
-// Configure Moonboots to serve our client application
-// ---------------------------------------------------
-var clientApp = new Moonboots({
-    moonboots: {
-        jsFileName: appName,
-        cssFileName: appName,
-        main: fixPath('clientapp/app.js'),
-        developmentMode: argv('local'),
-        buildDirectory: buildDirectory,
-        resourcePrefix: argv('build') || argv('pages') ? '/assets/' : '/',
-        libraries: [
-            fixPath('clientapp/libraries/google-analytics.js'),
-            fixPath('clientapp/libraries/raf.js'),
-            fixPath('node_modules/jquery/dist/jquery.js'),
-            fixPath('clientapp/libraries/typeahead.bundle.js'),
-            // Bootstrap modules
-            fixPath('clientapp/libraries/bootstrap/transition.js'),
-            fixPath('clientapp/libraries/bootstrap/alert.js'),
-            fixPath('clientapp/libraries/bootstrap/button.js'),
-            fixPath('clientapp/libraries/bootstrap/dropdown.js'),
-            fixPath('clientapp/libraries/bootstrap/collapse.js'),
-            fixPath('clientapp/libraries/bootstrap/modal.js'),
-            fixPath('clientapp/libraries/bootstrap/affix.js'),
-            fixPath('clientapp/libraries/bootstrap/tooltip.js'),
-            fixPath('clientapp/libraries/bootstrap/popover.js')
-        ],
-        stylesheets: [
-            fixPath('styles/app.css')
-        ],
-        beforeBuildJS: build.js,
-        beforeBuildCSS: build.css
+var moonbootsConfig = {
+    jsFileName: appName,
+    cssFileName: appName,
+    main: fixPath('clientapp/app.js'),
+    developmentMode: argv('local'),
+    resourcePrefix: argv('build') || argv('pages') ? '/assets/' : '/',
+    libraries: [
+        fixPath('clientapp/libraries/google-analytics.js'),
+        fixPath('clientapp/libraries/raf.js'),
+        fixPath('node_modules/jquery/dist/jquery.js'),
+        fixPath('clientapp/libraries/typeahead.bundle.js'),
+        // Bootstrap modules
+        fixPath('clientapp/libraries/bootstrap/transition.js'),
+        fixPath('clientapp/libraries/bootstrap/alert.js'),
+        fixPath('clientapp/libraries/bootstrap/button.js'),
+        fixPath('clientapp/libraries/bootstrap/dropdown.js'),
+        fixPath('clientapp/libraries/bootstrap/collapse.js'),
+        fixPath('clientapp/libraries/bootstrap/modal.js'),
+        fixPath('clientapp/libraries/bootstrap/affix.js'),
+        fixPath('clientapp/libraries/bootstrap/tooltip.js'),
+        fixPath('clientapp/libraries/bootstrap/popover.js')
+    ],
+    stylesheets: [
+        fixPath('styles/app.css')
+    ],
+    beforeBuildJS: function () {
+        templatizer(fixPath('clienttemplates'), fixPath('clientapp/templates.js'));
     },
-    handlers: {
-        html: function (cb) {
-            htmlSource.call(this, cb);
-        }
-    },
-    server: expressApp
-});
+    beforeBuildCSS: function (cb) {
+        lessitizer({
+            files: path.resolve(__dirname, 'styles/app.less'),
+            outputDir: path.resolve(__dirname, 'styles')
+        }, cb);
+    }
+};
 
-clientApp.dataFileName = dataFileName;
-clientApp.dataString = dataString;
-
-
-// ---------------------------------------------------
-// Build to deploy directory if CLI flag is set
-// ---------------------------------------------------
 if (argv('build')) {
-    console.log('Starting build');
-    return build.static(clientApp, appName, '_deploy', function () {
-        process.exit(0);
+    new Static({
+        moonboots: moonbootsConfig,
+        public: fixPath('public'),
+        directory: fixPath('_deploy'),
+        htmlSource: htmlSource,
+        cb: function () {
+            process.exit(0);
+        }
+    });
+} else {
+    var moonboots = new Moonboots({
+        moonboots: moonbootsConfig,
+        render: function () {
+            htmlSource.call(this);
+        },
+        server: expressApp
     });
 }
 
 
-// ---------------------------------------------------
-// Build to pages directory if CLI flag is set
-// ---------------------------------------------------
 if (argv('pages')) {
-    build.pages(clientApp, appName, '_pages', function () {
-        process.exit(0);
+    var crawler = new Crawler({
+        rndr: {
+            readyEvent: 'rendered'
+        },
+        app: 'http://localhost:3000/'
+    });
+    var pagesDir = fixPath('_pages');
+    var pagesIndex = path.join(pagesDir, 'index.html');
+
+    moonboots.on('ready', function () {
+        crawler.start().crawler.on('spaurl', function (url) {
+            console.log('Add', url);
+
+            var urlFile = path.basename(url);
+            var pageFile = path.join(pagesDir, urlFile) + '.html';
+            var pageDir = path.dirname(url);
+
+            mkdirp.sync(pageDir);
+            sh.run(['cp', pagesIndex, pageFile].join(' '));
+
+        }).on('complete', crawler.stop.bind(crawler, true));
     });
 }
 
