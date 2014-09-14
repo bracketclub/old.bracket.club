42753db3c0cbebe7c385c05d2ef6f1f078b403f9
Luke Karrys <lukekarrys@gmail.com>
2014-09-14 16:40:35 -0700

    bootstrap

---
+++ build/bootstrap.js
@@ -0,0 +1,39 @@
+var path = require('path');
+var fs = require('fs');
+var fp = require('./fixpath');
+
+
+function toImports (arr) {
+    if (!Array.isArray(arr)) {
+        arr = [arr];
+    }
+    return '\n' + arr.map(function (line) {
+        return '@import "' + line + '";';
+    }).join('\n');
+}
+
+function Bootstrap(options) {
+    options || (options = {});
+    this.override = toImports(options.override);
+    this.append = toImports(options.append);
+}
+
+Bootstrap.prototype.build = function () {
+    var res = fs.readFileSync(path.resolve(this.pathToBootstrap()), 'utf8');
+    res = res.replace(/^(@import ")(.*?)(";)$/gm, '$1' + fp(path.dirname(this.pathToBootstrap())) + '/$2$3');
+    res = res.split('\n');
+    res = res.map(function (line) {
+        if (line.indexOf('variables.less') > -1) {
+            line += this.override;
+        }
+        return line;
+    }, this).join('\n');
+    res += this.append;
+    return res;
+};
+
+Bootstrap.prototype.pathToBootstrap = function () {
+    return 'node_modules/bootstrap/less/bootstrap.less';
+};
+
+module.exports = Bootstrap;
