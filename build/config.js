8a43a7a608a8e9ee7d55b5130187bb222bf42a84
Luke Karrys <lukekarrys@gmail.com>
2014-09-14 16:40:35 -0700

    bootstrap

--- build/config.js
+++ build/config.js
@@ -3,6 +3,8 @@ var lessitizer = require('lessitizer');
 var templatizer = require('templatizer');
 var fixPath = require('./fixpath');
 var options = require('./options');
+var Bootstrap = require('./bootstrap');
+var fs = require('fs');
 
 
 // ------------------------
@@ -11,30 +13,40 @@ var options = require('./options');
 module.exports = {
     jsFileName: appName,
     cssFileName: appName,
-    main: fixPath('clientapp/app.js'),
+    main: fixPath('clientapp/app2.js'),
     developmentMode: !options.minify,
     resourcePrefix: options.build || options.crawl ? '/assets/' : '/',
     libraries: [
         fixPath('clientapp/libraries/google-analytics.js'),
         fixPath('node_modules/jquery/dist/jquery.js'),
         fixPath('clientapp/libraries/typeahead.bundle.js'),
-        // Bootstrap modules
-        fixPath('clientapp/libraries/bootstrap/transition.js'),
-        fixPath('clientapp/libraries/bootstrap/alert.js'),
-        fixPath('clientapp/libraries/bootstrap/button.js'),
-        fixPath('clientapp/libraries/bootstrap/dropdown.js'),
-        fixPath('clientapp/libraries/bootstrap/collapse.js'),
-        fixPath('clientapp/libraries/bootstrap/modal.js'),
-        fixPath('clientapp/libraries/bootstrap/affix.js'),
-        fixPath('clientapp/libraries/bootstrap/tooltip.js'),
-        fixPath('clientapp/libraries/bootstrap/popover.js')
+        fixPath('node_modules/bootstrap/js/transition.js'),
+        fixPath('node_modules/bootstrap/js/alert.js'),
+        fixPath('node_modules/bootstrap/js/button.js'),
+        fixPath('node_modules/bootstrap/js/dropdown.js'),
+        fixPath('node_modules/bootstrap/js/collapse.js'),
+        fixPath('node_modules/bootstrap/js/modal.js'),
+        fixPath('node_modules/bootstrap/js/affix.js'),
+        fixPath('node_modules/bootstrap/js/tooltip.js'),
+        fixPath('node_modules/bootstrap/js/popover.js')
     ],
     stylesheets: [fixPath('styles/app.css')],
     beforeBuildJS: function () {
         templatizer(fixPath('clienttemplates'), fixPath('clientapp/templates.js'));
     },
     beforeBuildCSS: function (cb) {
+        var b = new Bootstrap({
+            override: [
+                fixPath('styles/theme/variables.less'),
+                fixPath('styles/theme/override.less'),
+            ],
+            append: [
+                fixPath('styles/app/app.less')
+            ]
+        });
+        fs.writeFileSync(fixPath('styles/app.less'), b.build());
         lessitizer({
+            developmentMode: !options.minify,
             files: fixPath('styles/app.less'),
             outputDir: fixPath('styles')
         }, cb);
