627b774cd10f77d03529c9e6e895bc596db73e38
Luke Karrys <lukekarrys@gmail.com>
2015-03-02 01:19:58 -0700

    no listener mixin; ga; delete old code

--- build/config.js
+++ build/config.js
@@ -15,6 +15,7 @@ module.exports = {
     main: fixPath('clientapp/main.jsx'),
     developmentMode: !options.minify,
     resourcePrefix: (options.build || options.crawl) ? '/assets/' : '/',
+    libraries: [fixPath('clientapp/libraries/google-analytics.js')],
     stylesheets: [fixPath('styles/app.css')],
     browserify: {
         extensions: ['.jsx'],
