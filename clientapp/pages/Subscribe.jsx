d6f43ba237e258b5a7658fa87b3ed5aa552b7b00
Luke Karrys <lukekarrys@gmail.com>
2015-03-02 23:51:50 -0700

    subscribe page

--- clientapp/pages/Subscribe.jsx
+++ clientapp/pages/Subscribe.jsx
@@ -1,28 +1,40 @@
 let React = require('react');
 
+let Grid = require('react-bootstrap/lib/Grid');
+let Row = require('react-bootstrap/lib/Row');
+let Col = require('react-bootstrap/lib/Col');
+let Input = require('react-bootstrap/lib/Input');
+let Button = require('react-bootstrap/lib/Button');
+
 
 module.exports = React.createClass({
     render () {
-        return;
-        // form.modal-content(target="_blank", action="http://tweetyourbracket.us5.list-manage.com/subscribe/post", method="POST")
-        //   input(type="hidden", name="u", value="3357cbc15c95f163a6fff3a84")
-        //   input(type="hidden", name="id", value="2259ac644a")
-
-        //   .modal-header
-        //     button(type="button", class="close", data-dismiss="modal") &times;
-        //     h3.modal-title Stay up to date!
-
-        //   .modal-body.row
-        //     p.col-xs-12
-        //       strong Sign up to be notified on all the latest Tweet Your Bracket news.
-        //     .form-group.col-xs-12
-        //       input.form-control(type="email", autocapitalize="off", autocorrect="off", name="MERGE0", id="MERGE0", placeholder="Email (required)")
-        //     .form-group.col-xs-12.col-sm-6
-        //       input.form-control(type="text", name="MERGE1", id="MERGE1", placeholder="First name")
-        //     .form-group.col-xs-12.col-sm-6
-        //       input.form-control(type="text", name="MERGE2", id="MERGE2", placeholder="Last name")
-        //     p.col-xs-12 We'll never spam you or do bad things with your email. Promise.
-        //   .modal-footer
-        //     button.btn.btn-primary.btn-block.btn-lg(type="submit") Sign Me Up!
+        return (
+            <form target='_blank' action='http://tweetyourbracket.us5.list-manage.com/subscribe/post' method='POST'>
+                <input type='hidden' name='u' value='3357cbc15c95f163a6fff3a84' />
+                <input type='hidden' name='id' value='2259ac644a' />
+                <Grid>
+                    <Row>
+                        <Col xs={12}>
+                            <h3>Sign up to be notified on all the latest Tweet Your Bracket news.</h3>
+                            <p>We'll never spam you or do bad things with your email. Promise.</p>
+                        </Col>
+                        <Col xs={12} md={4} lg={3}>
+                            <Input type='email' autocapitalize='off' autocorrect='off' name='MERGE0' id='MERGE0' label='Email Address' />
+                        </Col>
+                        <Col xs={12} sm={6} md={4} lg={3}>
+                            <Input type='text' name='MERGE1' id='MERGE1' label='First name' />
+                        </Col>
+                        <Col xs={12} sm={6} md={4} lg={3}>
+                            <Input type='text' name='MERGE2' id='MERGE2' label='Last name' />
+                        </Col>
+                        <div className='clearfix' />
+                        <Col xs={12} md={2} lg={2}>
+                            <Button type='submit' block bsStyle='primary'>Submit</Button>
+                        </Col>
+                    </Row>
+                </Grid>
+            </form>
+        );
     }
 });
