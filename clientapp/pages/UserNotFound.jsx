b3218d015d693ebc9beafe3c3253d664cd264ba5
Luke Karrys <lukekarrys@gmail.com>
2015-03-02 23:59:11 -0700

    delete more old stuff

--- clientapp/pages/UserNotFound.jsx
+++ clientapp/pages/UserNotFound.jsx
@@ -6,12 +6,14 @@ module.exports = React.createClass({
     mixins: [State],
     render () {
         let user = this.props.user || this.getParams().user;
-        return (<div>
-            <h2>User not found</h2>
-            <p>
-                The user <strong>{user}</strong> does not exist.
-                Check out the <Link to='results'>results page</Link> for a full list of users.
-            </p>
-        </div>);
+        return (
+            <div>
+                <h2>User not found</h2>
+                <p>
+                    The user <strong>{user}</strong> does not exist.
+                    Check out the <Link to='results'>results page</Link> for a full list of users.
+                </p>
+            </div>
+        );
     }
 });
