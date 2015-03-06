e18fc3ea766e8eea53a54f6ddf0af7b03d775710
Luke Karrys <lukekarrys@gmail.com>
2015-03-06 00:34:25 -0700

    test with all data

--- build/data.js
+++ build/data.js
@@ -1,20 +1,14 @@
 var crypto = require('crypto');
 var config = require('figs');
-var _ = require('lodash');
 
-
-var year = process.env.TYB_YEAR || config.year;
 var sport = process.env.TYB_SPORT || config.sport;
-var liveData = require('bracket-data-live')({year: year, sport: sport});
-liveData.sportYear = {
-    year: year,
-    sport: sport
-};
-liveData.entriesByName = _.chain(liveData.entries).map(function (entry) {
-    return [entry.username.toLowerCase(), entry];
-}).object().value();
+var data = {sportYear: {year: '2015', sport: sport}};
 
-var dataString = 'window.bootstrap=' + JSON.stringify(liveData) + ';';
+['2012', '2013', '2014', '2015'].forEach(function (year) {
+    data[year] = require('bracket-data-live/data/' + sport + '/' + year);
+});
+
+var dataString = 'window.bootstrap=' + JSON.stringify(data) + ';';
 var dataHash = crypto.createHash('sha1').update(dataString).digest('hex').slice(0, 8);
 var dataFilename = 'data.' + dataHash + '.js';
 
