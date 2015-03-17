fff662771a9ecd0b299601a040e3b1eea458dda7
Luke Karrys <lukekarrys@gmail.com>
2015-03-17 00:08:08 -0700

    initial entry url; fix landing page

--- client/pages/Landing.jsx
+++ client/pages/Landing.jsx
@@ -8,6 +8,7 @@ let Bracket = require('../components/bracket/Container');
 let {rYear} = require('../global');
 let bracketHelpers = require('../helpers/bracket');
 
+let bracketEntryActions = require('../actions/bracketEntryActions');
 let bracketEntryStore = require('../stores/bracketEntryStore');
 let masterStore = require('../stores/masterStore');
 
@@ -16,29 +17,25 @@ module.exports = React.createClass({
     mixins: [State, ListenerMixin],
 
     componentDidMount() {
+        // Update store to contain the bracket from the url
+        // Note: store protects against falsy and bad values
+        bracketEntryActions.updateBracket(this.getParams().path);
         this.listenTo(bracketEntryStore, this.onChange);
         this.listenTo(masterStore, this.onChange);
-        // Add the bracket from the url to the bracket entry store
-        // only on the initial load (since the router action is null)
-        // TODO: yada yada yada, is this the Right Way?
-        // let {regex} = bracketHelpers({sport, year: props.year});
-        // if (router.action === null && regex.test(router.params.path)) {
-        //     bracketEntryActions.updateBracket(router.params.path);
-        // }
     },
 
     onChange () {
         this.setState(this.getInitialState());
     },
 
-    getInitialState () {
-        let {locked} = this.props;
+    getInitialState (props) {
+        let {locked} = (props || this.props);
         let {history, index} = (locked ? masterStore : bracketEntryStore).getState();
         return {history, index, urlParam: this.getParams().path};
     },
 
-    componentWillReceiveProps () {
-        this.setState(this.getInitialState());
+    componentWillReceiveProps (nextProps) {
+        this.setState(this.getInitialState(nextProps));
     },
 
     render () {
@@ -47,18 +44,16 @@ module.exports = React.createClass({
         let {regex} = bracketHelpers({sport, year});
 
         // The landing page is a few things dependent on state & url:
-
-        // A locked master bracket for a previous year
-        if (locked && rYear.test(urlParam)) {
+        if (locked) {
+            // A locked master bracket for a previous year
             return <Bracket {...this.props} history={stateHistory[year]} index={index} />;
+        }
+        else if (!urlParam || rYear.test(urlParam) || regex.test(urlParam)) {
+            // The current unlocked entry
+            return <Bracket {...this.props} history={stateHistory} index={index} />;
         }
 
         // A fallback url which will render the 404 for bad params
-        if (urlParam && !regex.test(urlParam)) {
-            return <FourOhFour />;
-        }
-
-        // The current unlocked entry
-        return <Bracket {...this.props} history={stateHistory} index={index} />;
+        return <FourOhFour />;
     }
 });
