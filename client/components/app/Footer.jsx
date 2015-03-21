8595c4f8421a54a83d2046ae2e268c00baad4872
Luke Karrys <lukekarrys@gmail.com>
2015-03-21 16:48:04 -0700

    sticky footer

--- client/components/app/Footer.jsx
+++ client/components/app/Footer.jsx
@@ -11,21 +11,23 @@ let Footer = React.createClass({
         let {me} = this.props;
         return (
             <footer>
-                <ul className='footer-links'>
-                    <li><a href='https://twitter.com/tweetthebracket'>Twitter</a></li>
-                    <li className='muted'>&middot;</li>
-                    <li><a href='https://github.com/tweetyourbracket'>GitHub</a></li>
-                    <li className='muted'>&middot;</li>
-                    <li><a href='http://lukecod.es/2014/01/25/tweet-your-bracket/'>What Is This?</a></li>
-                    <li className='follow-button'>
-                        <a className='twitter-follow-button' href='https://twitter.com/TweetTheBracket' data-show-count="false">
-                            Follow @TweetTheBracket
-                        </a>
-                    </li>
-                </ul>
-                <p>
-                    Made with love by <a href='https://twitter.com/lukekarrys'>{me.id === '23918006' ? 'You (you\'re great!)' : 'Luke'}</a> in Arizona.
-                </p>
+                <div className={this.props.className}>
+                    <ul className='footer-links'>
+                        <li><a href='https://twitter.com/tweetthebracket'>Twitter</a></li>
+                        <li className='muted'>&middot;</li>
+                        <li><a href='https://github.com/tweetyourbracket'>GitHub</a></li>
+                        <li className='muted'>&middot;</li>
+                        <li><a href='http://lukecod.es/2014/01/25/tweet-your-bracket/'>What Is This?</a></li>
+                        <li className='follow-button'>
+                            <a className='twitter-follow-button' href='https://twitter.com/TweetTheBracket' data-show-count="false">
+                                Follow @TweetTheBracket
+                            </a>
+                        </li>
+                    </ul>
+                    <p>
+                        Made with love by <a href='https://twitter.com/lukekarrys'>{me.id === '23918006' ? 'You (you\'re great!)' : 'Luke'}</a> in Arizona.
+                    </p>
+                </div>
             </footer>
         );
     }
