16aaf4a8ddf0572501a51ffd91bad58ee8fb5a2d
Luke Karrys <lukekarrys@gmail.com>
2015-03-22 10:31:34 -0700

    sample loading phrases

--- client/components/Loading.jsx
+++ client/components/Loading.jsx
@@ -1,11 +1,17 @@
 let React = require('react');
 
+let sample = require('lodash/collection/sample');
+let phrases = [
+    'Loading busted brackets',
+    'Searching for a perfect bracket'
+];
+
 
 let Loading = React.createClass({
     render () {
         return (
             <div className='page-loader'>
-                <h2>Loading busted brackets...</h2>
+                <h2>{sample(phrases)}...</h2>
                 <div className='ball-container'>
                     <div className='ball' />
                 </div>
