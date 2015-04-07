5750937bc29fbe032b3cd02d4087f997633545a2
Luke Karrys <lukekarrys@gmail.com>
2015-04-06 22:21:52 -0700

    update build env vars and static url sha

--- client/global.js
+++ client/global.js
@@ -14,7 +14,7 @@ module.exports = {
     activeYear,
     rYear: {test: (year) => years.indexOf((year || '').toString()) > -1},
     years: years,
-    staticUrl: 'https://cdn.rawgit.com/tweetyourbracket/api/86cb99e67fe7149cfe88a250992e1454733cb089/data-static/ncaa-mens-basketball',
+    staticUrl: 'https://cdn.rawgit.com/tweetyourbracket/api/6d84c44c991d8250515047655d2a8ed1ae19ee9d/data-static/ncaa-mens-basketball',
     apiUrl: 'http://104.236.223.8',
     firebase: new Firebase('https://tweetyourbracket.firebaseio.com')
 };
