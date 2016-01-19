8aaeeed2552b5d7ce277bd0733b69bcccdf2044a
Luke Karrys <lukekarrys@gmail.com>
2016-01-19 00:31:03 -0800

    analytics

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ import {createHistory} from 'history';
 import {Provider} from 'react-redux';
 import {syncHistory} from 'redux-simple-router';
 
+import ga from './lib/analytics';
 import firebase from './lib/firebase';
 import configureStore from './store/configureStore';
 import routes from './routes';
@@ -20,6 +21,10 @@ const reduxRouterMiddleware = syncHistory(history);
 const store = configureStore({middleware: [reduxRouterMiddleware]});
 
 reduxRouterMiddleware.listenForReplays(store);
+
+if (process.env.NODE_ENV === 'production') {
+  history.listen(({pathname}) => ga.pageview(pathname));
+}
 
 // Firebase will trigger the action if the user is logged in from a previous
 // session when first loading the page
