b926cd43fc8e5f434f7d7b17076bae68f7767eff
Luke Karrys <lukekarrys@gmail.com>
2016-01-20 15:46:32 -0700

    redo routes a bit; get bracket url working for entry

--- src/pages/FourOhFour.js
+++ src/pages/FourOhFour.js
@@ -12,7 +12,9 @@ export default class FourOhFour extends Component {
         <h2>Page not found</h2>
         <p>Whoops, something went wrong, which is totally my bad.</p>
         <p>
-          Not sure exactly how this happened, but you're best bet would be to head back to the <Link to='/'>home page</Link>. That's where most of the cool stuff is.
+          Not sure exactly how this happened, but you're best bet would be to head back to the <Link to='/'>home page</Link>.
+          {' '}
+          That's where most of the cool stuff is.
         </p>
       </Page>
     );
