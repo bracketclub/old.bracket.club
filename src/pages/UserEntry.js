590cb62edd7e42e08d8f5085bc716e3ffd7a0afd
Luke Karrys <lukekarrys@gmail.com>
2016-01-24 09:20:54 -0700

    make lookup entry like user entry; fix event header dropdown

--- src/pages/UserEntry.js
+++ src/pages/UserEntry.js
@@ -11,12 +11,17 @@ import * as mastersSelectors from '../selectors/masters';
 
 import Page from '../components/containers/Page';
 import DiffBracket from '../components/bracket/DiffBracket';
+import BracketNav from '../components/bracket/Nav';
+import BracketProgress from '../components/bracket/Progress';
+import BracketHeader from '../components/bracket/Header';
 import LockMessage from '../components/bracket/LockMessage';
 import UserInfo from '../components/user/Info';
 
 const mapStateToProps = (state, props) => ({
   diff: bracketSelectors.diff(state, props),
   master: mastersSelectors.bracketString(state, props),
+  navigation: mastersSelectors.navigation(state, props),
+  progress: mastersSelectors.progress(state, props),
   entry: entriesSelectors.currentWithUser(state, props),
   sync: mergeSyncState(state.entries, state.masters)
 });
@@ -33,16 +38,26 @@ export default class UserEntry extends Component {
     diff: PropTypes.func,
     entry: PropTypes.object,
     master: PropTypes.string,
-    sync: PropTypes.object
+    sync: PropTypes.object,
+    navigation: PropTypes.object,
+    progress: PropTypes.object
   };
 
   static getEventPath = (e, params) => `${e}/users/${params.userId}`;
 
+  handleNavigate = (method) => {
+    this.props.dispatch(mastersActions[method]());
+  };
+
   render() {
-    const {sync, entry, master, diff, locks, event, locked} = this.props;
+    const {sync, entry, master, diff, locks, event, locked, navigation, progress} = this.props;
 
     return (
       <Page sync={sync} width='full'>
+        <BracketHeader>
+          <BracketNav navigation={navigation} event={event} onNavigate={this.handleNavigate} />
+          <BracketProgress message='games played' progress={progress} />
+        </BracketHeader>
         <LockMessage locked={locked} locks={locks} event={event} />
         <UserInfo user={entry.user} />
         <DiffBracket {...{diff, entry: entry.bracket, master}} />
