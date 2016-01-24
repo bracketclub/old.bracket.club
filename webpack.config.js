5ed7844523dab96dc7c80e92e3687df484f4e7e0
Luke Karrys <lukekarrys@gmail.com>
2016-01-24 09:30:58 -0700

    ios web app; closes #74

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,7 @@ const renderHTML = (context) =>
       <title>Tweet Your Bracket</title>
       <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0">
       <meta name="apple-mobile-web-app-capable" content="yes">
+      <meta name="apple-mobile-web-app-status-bar-style" content="default">
       <link rel="stylesheet" href="/${context.css}">
   </head>
   <body>
