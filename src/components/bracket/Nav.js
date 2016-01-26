5c9926569332a18f740094a5ebf22c12ee9f55d5
Luke Karrys <lukekarrys@gmail.com>
2016-01-25 21:50:18 -0700

    nav message when not viewing latest game; closes #38

--- src/components/bracket/Nav.js
+++ src/components/bracket/Nav.js
@@ -1,5 +1,5 @@
 import React, {PropTypes, Component} from 'react';
-import {Glyphicon, Button, ButtonToolbar, ButtonGroup} from 'react-bootstrap';
+import {Glyphicon, Button, ButtonToolbar, ButtonGroup, OverlayTrigger, Tooltip} from 'react-bootstrap';
 
 export default class BracketNav extends Component {
   static propTypes = {
@@ -9,34 +9,59 @@ export default class BracketNav extends Component {
     onNavigate: PropTypes.func.isRequired
   };
 
+  getForwardButton() {
+    const {navigation, onGenerate, onReset, onNavigate} = this.props;
+    const {canGoForward} = navigation;
+    const canManipulate = onGenerate && onReset;
+
+    const button = (
+      <Button
+        bsStyle={!canManipulate && canGoForward ? 'primary' : 'default'}
+        disabled={!canGoForward}
+        onClick={() => canGoForward && onNavigate('goToLast')}
+      >
+        <Glyphicon glyph='fast-forward' />
+      </Button>
+    );
+
+    if (canManipulate) {
+      return button;
+    }
+
+    return (
+      <OverlayTrigger placement='bottom' overlay={<Tooltip>Click here to jump to the last game from this event.</Tooltip>}>
+        {button}
+      </OverlayTrigger>
+    );
+  }
+
   render() {
     const {navigation, onGenerate, onNavigate, onReset} = this.props;
     const {canGoBack, canGoForward, canReset} = navigation;
+    const canManipulate = onGenerate && onReset;
 
     return (
       <ButtonToolbar className='bracket-nav'>
         <ButtonGroup>
-          <Button disabled={!canGoBack} onClick={() => canGoBack && onNavigate('goToFirst')}>
+          <Button bsStyle='default' disabled={!canGoBack} onClick={() => canGoBack && onNavigate('goToFirst')}>
             <Glyphicon glyph='fast-backward' />
           </Button>
-          <Button disabled={!canGoBack} onClick={() => canGoBack && onNavigate('goToPrevious')}>
+          <Button bsStyle='default' disabled={!canGoBack} onClick={() => canGoBack && onNavigate('goToPrevious')}>
             <Glyphicon glyph='step-backward' />
           </Button>
-          <Button disabled={!canGoForward} onClick={() => canGoForward && onNavigate('goToNext')}>
+          <Button bsStyle='default' disabled={!canGoForward} onClick={() => canGoForward && onNavigate('goToNext')}>
             <Glyphicon glyph='step-forward' />
           </Button>
-          <Button disabled={!canGoForward} onClick={() => canGoForward && onNavigate('goToLast')}>
-            <Glyphicon glyph='fast-forward' />
-          </Button>
+          {this.getForwardButton()}
         </ButtonGroup>
-        {onGenerate && onReset &&
+        {canManipulate &&
           <ButtonGroup>
-            <Button disabled={!canReset} onClick={() => canReset && onReset('reset')}>
+            <Button bsStyle='default' disabled={!canReset} onClick={() => canReset && onReset('reset')}>
               <Glyphicon glyph='ban-circle' />
             </Button>
-            <Button onClick={() => onGenerate('lower')}>1</Button>
-            <Button onClick={() => onGenerate('higher')}>16</Button>
-            <Button onClick={() => onGenerate('random')}>
+            <Button bsStyle='default' onClick={() => onGenerate('lower')}>1</Button>
+            <Button bsStyle='default' onClick={() => onGenerate('higher')}>16</Button>
+            <Button bsStyle='default' onClick={() => onGenerate('random')}>
               <Glyphicon glyph='random' />
             </Button>
           </ButtonGroup>
