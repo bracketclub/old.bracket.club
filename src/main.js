4d01249e6c231ab0d2148ab2c7d5d18c82ac5528
Luke Karrys <lukekarrys@gmail.com>
2016-02-01 23:15:14 -0700

    fastclick

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import '../styles/index.less';
 import 'file?name=favicon.ico!../public/favicon.ico';
 
+import 'react-fastclick';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, browserHistory} from 'react-router';
