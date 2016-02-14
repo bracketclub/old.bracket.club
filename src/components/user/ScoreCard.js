48fbb1cfb84157d6b7486724d0b0d1ee5a682f94
Luke Karrys <lukekarrys@gmail.com>
2016-02-14 13:41:12 -0700

    lint

--- src/components/user/ScoreCard.js
+++ src/components/user/ScoreCard.js
@@ -17,7 +17,8 @@ export default class ScoreCard extends Component {
     return (
       <div className='score-card'>
         <p>
-          <strong>Total: </strong>{score.standard} {String.fromCharCode(183)} <strong>PPR: </strong>{score.standardPPR}<br/>
+          <strong>Total: </strong>{score.standard} {String.fromCharCode(183)} <strong>PPR: </strong>{score.standardPPR}
+          <br />
           <strong>Gooley: </strong>{score.gooley} {String.fromCharCode(183)} <strong>PPR: </strong>{score.gooleyPPR}
         </p>
       </div>
