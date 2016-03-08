344a73df4aa0f64d3ca2636329489c1a4f4d55ca
Luke Karrys <lukekarrys@gmail.com>
2016-03-07 22:31:46 -0700

    Update Footer.js

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -9,8 +9,6 @@ export default class Footer extends Component {
           <li><a href='https://twitter.com/tweetthebracket'>Twitter</a></li>
           <li className='muted'>{charCodes.dot}</li>
           <li><a href='https://github.com/tweetyourbracket'>GitHub</a></li>
-          <li className='muted'>{charCodes.dot}</li>
-          <li><a href='http://lukecod.es/2014/01/25/tweet-your-bracket/'>What Is This?</a></li>
         </ul>
         <p className='twitter'>
           <a className='twitter-follow-button' href='https://twitter.com/TweetTheBracket' data-show-count='false'>
