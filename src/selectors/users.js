88136fc0f1ed94527e8934601ae7ce2879a1c705
Luke Karrys <lukekarrys@gmail.com>
2016-03-10 11:17:19 -0700

    update deps

--- src/selectors/users.js
+++ src/selectors/users.js
@@ -1,5 +1,5 @@
 import {createSelector} from 'reselect';
-import {find, pick, property, compact} from 'lodash';
+import {find, pick, property, compact, orderBy, groupBy, map} from 'lodash';
 
 import eventInfo, {eventId} from './event';
 import * as bracketSelectors from './bracket';
@@ -12,7 +12,12 @@ const entries = (state, props) => state.entries.entities;
 const userId = (state, props) => props.params.userId;
 const userEventId = createSelector(userId, eventId, (...args) => args.join('/'));
 
-const mapUserToEntries = ($user, $entries) => compact(($user.entries || []).map((id) => $entries[id]));
+const mapUserToEntries = ($user, $entries) => {
+  const $userEntries = compact(($user.entries || []).map((id) => $entries[id]));
+  // Only include the latest entry for each year
+  const grouped = groupBy(orderBy($userEntries, 'created', 'desc'), ({sport, year}) => sport + year);
+  return map(grouped, (value) => value[0]);
+};
 
 export const userWithEntries = createSelector(
   visibleSelectors.byId(STATE_KEY, userId),
