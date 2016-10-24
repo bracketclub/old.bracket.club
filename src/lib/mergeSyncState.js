3ea3b5bcb7291eac31adbef0609a4bcc1d4e9921
Luke Karrys <lukekarrys@gmail.com>
2016-10-24 12:45:35 -0700

    fix merge sync state error; deps

--- src/lib/mergeSyncState.js
+++ src/lib/mergeSyncState.js
@@ -1,6 +1,6 @@
 export default (...selectors) => (...args) => selectors.reduce((res, selector) => {
   const state = (typeof selector.sync === 'function' ? selector.sync : selector)(...args);
-  res.syncing = res.syncing || state.syncing;
-  res.fetchError = res.fetchError || state.fetchError;
+  res.syncing = res.syncing || (state ? state.syncing : false);
+  res.fetchError = res.fetchError || (state ? state.fetchError : false);
   return res;
 }, {syncing: false, fetchError: null});
