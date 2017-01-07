29ed844a05080f2292fdd81e370ff3989963af7e
Luke Karrys <lukekarrys@gmail.com>
2017-01-07 13:29:32 -0700

    update normalizr with breaking changes and build non static version

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,20 +1,19 @@
 import {combineReducers} from 'redux';
 import {routerReducer} from 'react-router-redux';
 
-import {masters, entries} from '../schema';
+import {masters, entries, users} from '../schema';
 import me from './me';
 import event from './event';
 import entry from './entry';
 import canWin from './canWin';
-import users from './users';
 import endpointCreator from './endpoint';
 
 export default combineReducers({
   me,
   event,
   entry,
-  users,
   canWin,
+  users: endpointCreator(users),
   masters: endpointCreator(masters),
   entries: endpointCreator(entries),
   routing: routerReducer
