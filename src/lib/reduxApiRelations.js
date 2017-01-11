125afa07bc997a1456a049d7a10ab2f27107afd1
Luke Karrys <lukekarrys@gmail.com>
2017-01-10 17:53:56 -0700

    test with jest

--- src/lib/reduxApiRelations.js
+++ src/lib/reduxApiRelations.js
@@ -1,7 +1,7 @@
 import actionNames from 'action-names';
 import {without} from 'lodash';
 
-import {ENTITIES} from './endpointReducer';
+import {ENTITIES, RESULT} from './endpointReducer';
 
 export default ({dispatch}) => (next) => (action) => {
   // The next action is run first because any related resources should
@@ -10,19 +10,44 @@ export default ({dispatch}) => (next) => (action) => {
   next(action);
 
   const {meta = {}, payload = {}} = action;
-  const {[ENTITIES]: entities} = payload || {};
-  const {resource} = meta || {};
+  const {[ENTITIES]: entities} = payload;
+  const {resource} = meta;
 
   if (entities && resource) {
-    without(Object.keys(entities), resource).forEach((resourceType) => {
+    const relations = without(Object.keys(entities), resource);
+
+    relations.forEach((relation) => {
+      const relatedEntities = entities[relation];
+      const success = actionNames(relation).fetchSuccess;
+
+      // Dispatch the related entities to the reducer
       dispatch({
-        type: actionNames(resourceType).fetchSuccess,
+        type: success,
         payload: {
-          [ENTITIES]: {
-            [resourceType]: entities[resourceType]
-          }
+          [ENTITIES]: {[relation]: relatedEntities},
+          [RESULT]: {}
         }
       });
+
+      // Of all the related entities, dispatch a result for each one so that
+      // the records reducer can be populated to know that this data is loaded
+      // for other possible pages
+      // TODO: this should only happen for entries -> users and not users -> entries
+      // as the former doesn't have enough data to fully cache the entries result
+      // Object.keys(relatedEntities).forEach((id) => {
+      //   dispatch({
+      //     type: success,
+      //     meta: {
+      //       ...meta,
+      //       resource: relation,
+      //       id: `${id}/${meta.id}`
+      //     },
+      //     payload: {
+      //       [ENTITIES]: {},
+      //       [RESULT]: id
+      //     }
+      //   });
+      // });
     });
   }
 };
