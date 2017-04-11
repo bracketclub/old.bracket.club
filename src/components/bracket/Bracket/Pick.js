f9259b11a86aa0a9131355f3f8d82b52853b6122
Luke Karrys <lukekarrys@gmail.com>
2017-04-11 12:37:42 -0700

    dropups

--- src/components/bracket/Bracket/Pick.js
+++ src/components/bracket/Bracket/Pick.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
-import {Dropdown, MenuItem} from 'react-bootstrap';
+import {Dropdown, MenuItem, SafeAnchor} from 'react-bootstrap';
 import {pick, get} from 'lodash';
 
 import Team from './Team';
@@ -14,11 +14,27 @@ export default class Pick extends Component {
     team: PropTypes.object,
     opponent: PropTypes.object,
     winner: PropTypes.object,
-    isWinner: PropTypes.bool
+    isWinner: PropTypes.bool,
+    top: PropTypes.bool
   };
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dropdownOpen: false
+    };
+  }
+
+  onToggle = (isOpen) => {
+    this.setState({
+      dropdownOpen: isOpen
+    });
+  }
+
   render() {
-    const {onUpdate, bestOf, team, opponent, winner, isWinner} = this.props;
+    const {onUpdate, bestOf, team, opponent, winner, isWinner, top} = this.props;
+    const {dropdownOpen} = this.state;
 
     // This is a clickable team because there's an onUpdate function
     // and a team currently in this slot
@@ -40,16 +56,29 @@ export default class Pick extends Component {
     };
 
     if (bestOf) {
+      const menuClass = cx(styles.seriesMenu, {
+        [styles.seriesMenuOpen]: dropdownOpen,
+        [styles.seriesMenuUp]: !top
+      });
+
       return (
-        <Dropdown className={styles.teamDropdown} id='wins-in-dropdown'>
+        <Dropdown className={styles.seriesPicker} open={dropdownOpen} onToggle={this.onToggle} id='wins-in-dropdown'>
           <Team bsRole='toggle' className={cx(styles.team, styles.pickable)} {...team}>
             {isWinner && get(winner, 'winsIn') &&
               <span className={styles.winsIn}>{winner.winsIn}</span>
             }
           </Team>
-          <Dropdown.Menu>
+          <Dropdown.Menu className={menuClass}>
             {bestOf.range.map((winsIn) => (
-              <MenuItem key={winsIn} eventKey={winsIn} onClick={(e) => handler(e, {winsIn})}>{winsIn}</MenuItem>
+              <MenuItem
+                className={styles.seriesItem}
+                key={winsIn}
+                eventKey={winsIn}
+                onClick={(e) => handler(e, {winsIn})}
+                componentClass={(props) => <SafeAnchor {...props} className={styles.seriesItemInner} />}
+              >
+                {winsIn}
+              </MenuItem>
             ))}
           </Dropdown.Menu>
         </Dropdown>
