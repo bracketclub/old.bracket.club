5804c7c73a1f350ef6c6c38dff398cae02fb79f5
Luke Karrys <lukekarrys@gmail.com>
2017-05-09 19:00:20 -0700

    webpack 2

---
+++ webpack/html.js
@@ -0,0 +1,25 @@
+const html = require('html-tagged-literals');
+
+module.exports = (context, isDev) => html[isDev ? 'unindent' : 'minify']`
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <title>Bracket Club</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="default">
+    <meta name="apple-mobile-web-app-title" content="Brackt Club">
+    <link rel="shortcut icon" href="/favicon.ico">
+    <link rel="icon" href="/favicon.png">
+    <link rel="icon" sizes="192x192" href="/favicon-192x192.png">
+    <link rel="apple-touch-icon-precomposed" href="/favicon-192x192.png">
+    <link rel="stylesheet" href="/${context.css}">
+  </head>
+  <body>
+    <div id='root'></div>
+    <script>window.twttr=(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],t=window.twttr||{};if(d.getElementById(id))return;js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);t._e=[];t.ready=function(f){t._e.push(f);};return t;}(document,"script","twitter-wjs"));</script>
+    <script src="/${context.main}"></script>
+  </body>
+  </html>
+`;
---
+++ webpack/styles.js
@@ -0,0 +1,38 @@
+const _ = require('lodash');
+
+module.exports = (config, {src, isDev}) => {
+  const removeLoaders = (removeExtensions) => {
+    config.module.rules.forEach((rule, index, rules) => {
+      const rm = removeExtensions.some((ext) => rule.test.source.endsWith(`.${ext}$`));
+      if (rm) rules[index] = {};
+    });
+  };
+
+  const addLoader = (rule, ruleOptions, loaderIndex, loaderOptions) => {
+    const cloneRule = _.cloneDeep(rule);
+    const loader = cloneRule.use[loaderIndex];
+    _.assign(cloneRule, ruleOptions);
+    cloneRule.use[loaderIndex] = {
+      loader: loader.loader || loader,
+      options: loaderOptions
+    };
+    config.module.rules.push(cloneRule);
+  };
+
+  // Save hjs less rule and then delete all style rules
+  const lessRule = config.module.rules.find((rule) => rule.test.source.endsWith('.less$'));
+  removeLoaders(['css', 'less']);
+
+  // We know where the css loader is located, this is fragile but way easier than looking for it
+  const cssIndex = isDev ? 1 : 2;
+
+  addLoader(lessRule, {include: [src]}, cssIndex, {
+    modules: true,
+    minimize: isDev ? false : {discardComments: {removeAll: true}},
+    localIdentName: `${isDev ? '[path][name]___[local]___' : ''}[hash:base64:5]`
+  });
+
+  addLoader(lessRule, {exclude: [src]}, cssIndex, {
+    minimize: isDev ? false : {discardComments: {removeAll: true}}
+  });
+};
