40126d746a665da587ba075f6eb5a5d1f8de3ead
Luke Karrys <lukekarrys@gmail.com>
2017-05-19 12:41:31 -0700

    set lock unlock state by default

--- src/actions/event.js
+++ src/actions/event.js
@@ -7,11 +7,17 @@ const lock = (id) => ({
   payload: {id}
 });
 
+const unlock = (id) => ({
+  type: actions.UNLOCK,
+  payload: {id}
+});
+
 export const countdown = (id) => (dispatch, getState) => {
   const locks = bracketSelectors.locks(getState(), {match: {params: {eventId: id}}});
   const now = Date.now();
 
   if (now < new Date(locks)) {
+    dispatch(unlock(id));
     rafCountdown(locks, () => dispatch(lock(id)));
   }
 };
