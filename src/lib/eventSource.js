945a0649aa083192d264df5b15e5bc96fd3aab0c
Luke Karrys <lukekarrys@gmail.com>
2021-03-15 15:00:43 -0700

    debug event source events

--- src/lib/eventSource.js
+++ src/lib/eventSource.js
@@ -3,10 +3,22 @@ import actionNames from 'lib/actionNames'
 
 const { EventSource } = window
 
+const debugLs =
+  typeof window.localStorage !== 'undefined' &&
+  window.localStorage.getItem('debug')
+const debugLog = (...args) => {
+  if (process.env.NODE_ENV !== 'production' || debugLs) {
+    // eslint-disable-next-line no-console
+    console.log(...args)
+  }
+}
+
 export default ({ endpoint, id, dispatch }, onData) => {
   if (!EventSource || !config.sse) return () => void 0
 
   const actionType = actionNames(endpoint).sse
+
+  const log = (...args) => debugLog(`[EventSource] ${endpoint} ${id}`, ...args)
 
   const dispatchEvent = (status) =>
     dispatch({
@@ -19,11 +31,21 @@ export default ({ endpoint, id, dispatch }, onData) => {
 
   const source = new EventSource(`${config.apiUrl}/${endpoint}/events`)
   const close = () => {
+    log(`closing listener`)
     dispatchEvent(false)
     source.close()
   }
 
-  source.addEventListener(id, (e) => onData(JSON.parse(e.data)), false)
+  log(`adding listener`)
+  source.addEventListener(
+    id,
+    (e) => {
+      const data = JSON.parse(e.data)
+      log(`data`, data)
+      onData(data)
+    },
+    false
+  )
   source.addEventListener('end', close)
 
   return close
